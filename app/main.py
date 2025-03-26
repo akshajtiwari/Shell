4c74b06d7b5c69137cf80bfa1f7557f1697b6c8f
akshajtiwari <akshajtiwari9a@gmail.com>
2025-03-26 19:43:26 +0530

    [REPL]

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ def main():
         sys.exit(0)
         break
      elif user_input.startswith("echo "):
-        print(user_input[4:0])
+        print(user_input[5:0])
      else:
         print(f"{user_input}: command not found")
     # Wait for user input
