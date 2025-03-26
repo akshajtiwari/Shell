dd7795a5b33197d428b768676e950ff64db3654a
akshajtiwari <akshajtiwari9a@gmail.com>
2025-03-26 21:47:35 +0530

    [REPL]

--- app/main.py
+++ app/main.py
@@ -6,20 +6,29 @@ def main():
      #sys.stdout.write("$ ")
     while True:
      sys.stdout.write("$ ")
+
      sys.stdout.flush()
+
      user_input = input()
+
      if user_input.lower()=="exit 0":
         sys.exit(0)
         break
+
      if user_input.startswith("echo "):
         print(user_input[5:])
+
      if user_input == 'type echo':
         print('echo is a shell builtin')
-     if user_input == 'exit':
+
+     if user_input == 'type exit':
         print('exit is a shell builtin')
+
      if user_input == 'invalid_command':
         print('invalid_command: not found')
+
     print(f"{user_input}: command not found")
+
     # Wait for user input
     input()
 
