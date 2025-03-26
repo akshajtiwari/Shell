2ae909f1493ef004f3f6771dce31230afff493cd
akshajtiwari <akshajtiwari9a@gmail.com>
2025-03-26 19:18:31 +0530

    [REPL]

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ import sys
 def main():
      #Uncomment this block to pass the first stage
      #sys.stdout.write("$ ")
-    sys.stdout.write("$ ")
     while True:
+     sys.stdout.write("$ ")
+     sys.stdout.flush
      user_input = input()
      if user_input.lower()=="exit":
         break
