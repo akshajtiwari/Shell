6be3d1bf8d0d17f4cbcfff11045ebe69c310264f
akshajtiwari <akshajtiwari9a@gmail.com>
2025-03-29 19:10:45 +0530

    [REPL]

--- app/main.py
+++ app/main.py
@@ -20,7 +20,6 @@ def run_executable(user_input):
 
     if executable_path:
         try:
-            
             result = subprocess.run([command] + args, capture_output=True, text=True)
             print(result.stdout, end="")  # Print output exactly as expected
         except Exception as e:
@@ -39,6 +38,8 @@ def main():
             sys.exit(0)
         elif user_input.startswith("echo "):
             print(user_input[5:])
+        elif user_input == "pwd":
+            print(os.getcwd()) 
         elif user_input.startswith("type "):
             command = user_input[5:]
             if command in {"echo", "exit", "type"}:
@@ -49,6 +50,7 @@ def main():
                     print(f"{command} is {path}")  # ✅ Print full path
                 else:
                     print(f"{command}: not found")
+
         else:
             run_executable(user_input)
 
